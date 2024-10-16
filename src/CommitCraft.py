d99b51909b6ae390132e98fa16d378c52748d339
Felix-Pedro <felix@auneria.com>
2024-10-16 19:43:00 -0300

    🎨 Introducing new features to the CommitCraft project by adding a `.env.example` file and modifying the `CommitCraft.py` file.
    The changes introduced in this commit are:
    * Added a new file `.env.example` with sample environment variables.
    * Modified the `CommitCraft.py` file to include new functionality for generating commit messages based on the provided context. The code now uses a different provider (OLLAMA) and model (LLama3.1) to generate more accurate and informative commit messages.

--- src/CommitCraft.py
+++ src/CommitCraft.py
@@ -1,10 +1,67 @@
-from git import Repo
+from ast import Raise
+import subprocess
 
-def get_diff():
-    repo = Repo(".")  # Assuming you are running this script in the root of your repository
-    index = repo.index
-    diff = index.diff("HEAD", create_patch=True)
-    return [str(diff) for diff in diff]
+from requests.api import options
+from defaults import default
 
-def commit_craft(diff, ):
-    pass
+def get_diff() -> str:
+    diff = subprocess.run(['git', 'diff', '--staged', '-M'], capture_output=True, text=True)
+    return diff.stdout
+
+def commit_craft(diff : str,
+                provider : str='ollama',
+                model : str='llama3.1',
+                provider_url : str | None=None,
+                context : str | None=None,
+                system_prompt : str | None=None,
+                user_prompt : str | None=None) -> str:
+    context_info = context
+    if context:
+        context = context.strip()
+        if context.startswith('file:'):
+            with open(context.lstrip('file:')) as file:
+                match context.split('.')[-1]:
+                    case 'toml':
+                        import toml
+                        context_info = toml.loads(file.read())
+                    case 'yml' | 'yaml':
+                        import yaml
+                        context_info = yaml.safe_load(file.read())
+                    case 'json':
+                        import json
+                        context_info = json.loads(file.read())
+                    case _:
+                        context_info = file.read()
+        else:
+            context_info=context
+    if user_prompt:
+        user_prompt = user_prompt.strip()
+    if system_prompt:
+        system_prompt = system_prompt.strip()
+    elif context:
+        if context_info and isinstance(context_info, dict):
+            system_prompt = f'''
+            # Proposure
+            You are a commit message helper for {context_info["name"]} a project written in {context_info["language"]} described as :
+            {context_info["description"]}
+
+            Your only task is to recive a git diff and return a conciveble commit message folowing those gidelines:
+
+            - Never ask for folow-up questions.
+            - Don't ask quetions.
+            - Don't talk about yourself.
+            - Be concise and clear.
+            - Be informative.
+            - Don't explain row by row just the global goal of the changes.
+            - Avoid unecessary details and long explanations.
+            - Use action verbs.
+            - Use bullet points in the body if there are many changes
+            {context_info.get("commit_guidelies", default.get("commit_guidelines"))}
+            {context_info.get("emoji", default.get("emoji"))}
+            '''
+    match provider:
+        case "ollama":
+            import ollama
+            return ollama.generate(model=model, system=system_prompt, prompt=diff, options={'num_ctx' : 32768})['response']
+        case _:
+            raise NotImplementedError("provider not found")
--- src/main.py
+++ src/main.py
@@ -1,3 +1,24 @@
-from CommitCraft import commit_craft
+import os
+from dotenv import load_dotenv
+from CommitCraft import commit_craft, get_diff
+import argparse
+
+load_dotenv()
+
 def main():
-    pass
+    diff = get_diff()
+    if os.path.exists('./.commitcraft/context.toml'):
+        context = 'file:./.commitcraft/context.toml'
+    elif os.path.exists('./.commitcraft/context.yaml'):
+        context = 'file:./.commitcraft/context.yaml'
+    elif os.path.exists('./.commitcraft/context.yml'):
+        context = 'file:./.commitcraft/context.yml'
+    elif os.path.exists('./.commitcraft/context.json'):
+        context = 'file:./.commitcraft/context.json'
+    else:
+        context = None
+
+    return commit_craft(diff, context=context)
+if __name__ == '__main__':
+    print(main())
+    
